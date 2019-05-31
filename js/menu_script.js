4f7682f39a6413272fee1154b4a9e99de701abb2
Niccolò Agnoletti <hsnicco@gmail.com>
2019-05-31 21:51:25 +0200

    up

---
+++ js/menu_script.js
@@ -0,0 +1,18 @@
+function toggleMenu(){
+	var NAV = document.getElementById("menu");
+
+	NAV.classList.toggle("flex-spcbtwn");
+	NAV.classList.toggle("hidden-mobile");
+	NAV.classList.toggle("flex-column");
+	NAV.classList.toggle("flex-center");
+	
+	var MENU = document.getElementById("toggle-menu");
+	var OPEN = "multimedia/png/open.png";
+	var CLOSE = "multimedia/png/close.png";
+	
+	if(MENU.src = OPEN){ 
+		MENU.src = CLOSE;
+	} else if (MENU.src == CLOSE){
+		MENU.src = OPEN;
+	} else { MENU.src = OPEN; };
+}
