d839aabf295bb70b3340f020735bfcaf4f489b59
Niccolò Agnoletti <hsnicco@gmail.com>
2020-04-20 00:28:02 +0200

    big update

---
+++ js/manu-visibility.js
@@ -0,0 +1,14 @@
+function toggleMenu(){
+
+	var MENU_BTN = document.getElementById("toggle-menu");
+
+	if (MENU_BTN.src.match(/open.png/) != null) { 
+		MENU_BTN.src.replace('open','close'); 
+	} else { MENU_BTN.src.replace('close','open'); }; 	
+}
+
+function toggleVisibility(primary,secondary){
+	secondary = secondary || 0;
+	document.getElementById(primary).classList.toggle('d-none');
+	if (secondary != 0) { document.getElementById(secondary).classList.toggle('d-none'); }
+}
